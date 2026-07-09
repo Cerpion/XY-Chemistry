d20e1f318a2230e843fa63e7b369764f78c5acef
andresmera-eng <andresfe_mera@hotmail.com>
2026-07-09 16:23:38 -0500

    [Add] Mecanica de acierto y Error
    Se compara las id de la recete y del pedido del cliente

--- Assets/Scripts/ClientOrder.cs
+++ Assets/Scripts/ClientOrder.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 public class ClientOrder : MonoBehaviour
 {
     [SerializeField] private List<ItemID> items = new List<ItemID>();
+    private ClientMovement clientMovement;
+    public bool hasOrderInProgress;
+    public ItemID costumeOrder;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hasOrderInProgress = false;
+        clientMovement = GetComponent<ClientMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (clientMovement.isInDeliveryStage && !hasOrderInProgress)
+        {
+            SelectOrder();
+            hasOrderInProgress = true;
+        }
+    }
+
+    private void SelectOrder()
+    {
+        int orderIndex = Random.Range(0, items.Count);
+        costumeOrder = items[orderIndex];
     }
 }
--- Assets/Scripts/RecipeMovement.cs
+++ Assets/Scripts/RecipeMovement.cs
@@ -11,9 +11,12 @@ public class RecipeMovement : MonoBehaviour
     [SerializeField] private float raiseSpeed;
     [SerializeField] private float fallSpeed;
     private Rigidbody componentRb;
+    public ItemID recipeID;
+    private GameManager gameManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         delieverStage = GameObject.FindGameObjectWithTag("DelieverStage");
         componentRb = GetComponent<Rigidbody>();
 
@@ -51,9 +54,14 @@ public class RecipeMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("DelieverStage"))
         {
             Debug.Log("Entro");
+            gameManager.CompareRecipes(recipeID);
             Destroy(gameObject);
         }
     }
     
+    public void DefineRecipeID(ItemID id)
+    {
+        recipeID = id;
+    }
 
 }
